66213e68feeb3ccdb2c09bc03678a98b46e4bfdd
Sebastian Haesselbarth <seb@sebmail.de>
2018-11-25 11:17:03 +0100

    Renaming to nibeuplink

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 /**
  *
- * template adapter
+ * nibeuplink adapter
  *
  *
  *  file io-package.json comments:
  *
  *  {
  *      "common": {
- *          "name":         "template",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
- *          "version":      "0.0.0",                    // use "Semantic Versioning"! see http://semver.org/
- *          "title":        "Node.js template Adapter",  // Adapter title shown in User Interfaces
+ *          "name":         "nibeuplink",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
+ *          "version":      "0.0.1",                    // use "Semantic Versioning"! see http://semver.org/
+ *          "title":        "Nibe Uplink Adapter",  // Adapter title shown in User Interfaces
  *          "authors":  [                               // Array of authord
- *              "name <mail@template.com>"
+ *              "Sebastian Haesselbarth <seb@sebmail.de>"
  *          ]
- *          "desc":         "template adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
+ *          "desc":         "Nibe Uplink Adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
  *          "platform":     "Javascript/Node.js",       // possible values "javascript", "javascript/Node.js" - more coming
- *          "mode":         "daemon",                   // possible values "daemon", "schedule", "subscribe"
+ *          "mode":         "schedule",                   // possible values "daemon", "schedule", "subscribe"
  *          "materialize":  true,                       // support of admin3
- *          "schedule":     "0 0 * * *"                 // cron-style schedule. Only needed if mode=schedule
+ *          "schedule":     "*\/5 * * * *"                 // cron-style schedule. Only needed if mode=schedule
  *          "loglevel":     "info"                      // Adapters Log Level
  *      },
  *      "native": {                                     // the native object is available via adapter.config in your adapters code - use it for configuration
@@ -38,8 +38,8 @@ const utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
 
 // you have to call the adapter function and pass a options object
 // name has to be set and has to be equal to adapters folder name and main file name excluding extension
-// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
-const adapter = new utils.Adapter('template');
+// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.nibeuplink.0
+const adapter = new utils.Adapter('nibeuplink');
 
 /*Variable declaration, since ES6 there are let to declare variables. Let has a more clearer definition where 
 it is available then var.The variable is available inside a block and it's childs, but not outside. 
@@ -105,7 +105,7 @@ function main() {
      *
      *      For every state in the system there has to be also an object of type state
      *
-     *      Here a simple template for a boolean variable named "testVariable"
+     *      Here a simple nibeuplink for a boolean variable named "testVariable"
      *
      *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
      *
@@ -121,7 +121,7 @@ function main() {
         native: {}
     });
 
-    // in this template all states changes inside the adapters namespace are subscribed
+    // in this nibeuplink all states changes inside the adapters namespace are subscribed
     adapter.subscribeStates('*');
 
 
