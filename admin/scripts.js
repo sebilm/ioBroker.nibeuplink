1646a2446af9f0e3d19f659ac17d8e05f91b9e9a
Sebastian Haesselbarth <seb@sebmail.de>
2022-02-06 13:03:10 +0100

    Code cleanup

--- admin/scripts.js
+++ admin/scripts.js
@@ -1,5 +1,10 @@
-function generateURL() {    
-    var callbackURL = document.getElementById("CallbackURL").value;
-    var identifier = document.getElementById("Identifier").value;
-    document.getElementById("URL").href = "https://api.nibeuplink.com/oauth/authorize?response_type=code&client_id="+identifier+"&scope=READSYSTEM%20WRITESYSTEM&redirect_uri="+callbackURL+"&state=init";
+function generateURL() {
+    var callbackURL = document.getElementById('CallbackURL').value;
+    var identifier = document.getElementById('Identifier').value;
+    document.getElementById('URL').href =
+        'https://api.nibeuplink.com/oauth/authorize?response_type=code&client_id=' +
+        identifier +
+        '&scope=READSYSTEM%20WRITESYSTEM&redirect_uri=' +
+        callbackURL +
+        '&state=init';
 }
